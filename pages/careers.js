265df330683a0d9774d9145d4b1eda680d76c01e
Domaine Scully <dalexscully@gmail.com>
2023-02-07 15:40:21 -0500

    Class demo React practice

---
+++ pages/careers.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Careers() {
+  return (
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl">Careers page Coming Soon</h1>
+        <Link href="/">Back to Home</Link>
+      </div>
+  )
+}
